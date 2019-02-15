2de8f3182d36155e5faa5bb6b3d273b255fa9593
Alexander Tikhonov <fatal1ty.rnd@gmail.com>
2019-02-15 18:27:39 +0300

    Change version to 0.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="aiobroker",
-    version="1.3",
+    version="0.1",
     description="Message broker for the communication between the components",
     platforms="all",
     classifiers=[
